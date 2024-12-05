b3f9e5b8d58c31441ba11a4791739653d42bcc89
Tomas Scagliarini <tomscag92@gmail.com>
2024-12-05 15:26:57 +0100

    Added context switch to save results

--- Analysis/main_correlations.py
+++ Analysis/main_correlations.py
@@ -19,8 +19,7 @@ def create_hdf_dataset(fnameout):
     fout.create_dataset("results", shape=(2664, 2664, 3), dtype="f")
     fout.attrs['finput'] = str(fileinput)
     # fout.attrs['finputsurr'] = str(finputsurr)
-
-    return fout
+    fout.close()
 
 
 def save_results(fout, i, j, Z, the_lagmax, prob):
@@ -39,20 +38,21 @@ def correlation_all(data, data_surr, fnameout):
         # This is to return a normalized cross-correlation
         data[:, i] = data[:, i]/np.sqrt(T)
 
-    for i in range(0, N):
-        print(f"Computing node {i}")
-        for j in range(i+1, N):
-            # print(j)
-            dist = haversine_distance(
-                nodes[i][0], nodes[i][1], nodes[j][0], nodes[j][1])
-            x = data[:, i]
-            y = data[:, j]
-            surr_x = data_surr[:, :, ind_nodes[i][0], ind_nodes[i][1]]
-            surr_y = data_surr[:, :, ind_nodes[j][0], ind_nodes[j][1]]
-
-            Z, prob, crossmax, the_lagmax = posterior_link_probability_iaaft(x, y, surr_x, surr_y,
-                                                                             dist, max_lag, num_surr=num_surr)
-            save_results(fout, i, j, Z, the_lagmax, prob)
+    with h5py.File(fnameout, mode='r+') as fout:
+        for i in range(0, N):
+            print(f"Computing node {i}")
+            for j in range(i+1, N):
+                # print(j)
+                dist = haversine_distance(
+                    nodes[i][0], nodes[i][1], nodes[j][0], nodes[j][1])
+                x = data[:, i]
+                y = data[:, j]
+                surr_x = data_surr[:, :, ind_nodes[i][0], ind_nodes[i][1]]
+                surr_y = data_surr[:, :, ind_nodes[j][0], ind_nodes[j][1]]
+    
+                Z, prob, crossmax, the_lagmax = posterior_link_probability_iaaft(x, y, surr_x, surr_y,
+                                                                                 dist, max_lag, num_surr=num_surr)
+                save_results(fout, i, j, Z, the_lagmax, prob)
 
 #############################
 
