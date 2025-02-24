cfb17921d8bd7b9100223872b6b246cdb83ac728
Tomas Scagliarini <tomscag92@gmail.com>
2025-02-24 12:11:49 +0100

    Joined the three network metrics in a single function

--- Analysis/lib/plot/AnalyzeFuzzyNetwork.py
+++ Analysis/lib/plot/AnalyzeFuzzyNetwork.py
@@ -10,7 +10,11 @@ from lib.misc import (
 
 class AnalyzeFuzzyNetwork():
 
-    def __init__(self, fnameinput, years) -> None:
+    def __init__(self, 
+                 fnameinput: str, 
+                 years: np.array,
+                 modes: list
+                 ) -> None:
         """
             Initialize oject attributes and create figure
         """
@@ -18,122 +22,104 @@ class AnalyzeFuzzyNetwork():
         self.resfolder = "./fig/"
         self.years = years
         
+        
         self.medium_fontsize = 20
         
-        # self.prb_mat = self.load_results(self.fnameinput, self.years, index=2)
-        # self.prb_mat = np.maximum(self.prb_mat, self.prb_mat.transpose())
-        # self.numyears = self.endyear - self.startyear + 1
+        self.modes = modes
+        self.initialize_modes()
 
-        # self.numfuzzy = 1  # Number of fuzzy networks to generate
+        
+    def plot(self, 
+             axes):
+        
+        for idx, year in enumerate(self.years):
+            print(f"Analyze - year {year}")
+            finput = glob.glob(self.fnameinput + 
+                               f"/*_year_{year}_maxlag_150.hdf5")[0]
+            
+            prb_mat = load_dataset_hdf5(finput, year, index=2)
+            prb_mat = np.maximum(prb_mat, prb_mat.transpose())
+            self.graph = sample_fuzzy_network(prb_mat)
+            
+            for mode in self.modes:
+                if mode == "clustering":
+                    self.clust[idx] = self.compute_clustering_coefficient(mode="gcc")
+                elif mode == "modularity":
+                    self.modularity[idx] = self.compute_modularity()
+                elif mode == "edge_density":
+                    self.density[idx] = self.compute_edge_density()
+                else:
+                    print("Type not recognized")
+        
+        for idx, mode in enumerate(self.modes):
+            
+            if mode == "clustering":
+                axes[idx].plot(self.years, self.clust)
+                axes[idx].set_ylabel("Clustering", fontsize=self.medium_fontsize)
+            elif mode == "modularity":
+                axes[idx].plot(self.years, self.modularity)
+                axes[idx].set_ylabel("Modularity", fontsize=self.medium_fontsize)
+            elif mode == "edge_density":
+                axes[idx].plot(self.years, self.density)
+                axes[idx].set_ylabel("Edge density", fontsize=self.medium_fontsize)
+            else:
+                raise ValueError("Type not recognized")
+                
+        return axes
+            
 
-        # self.fnameoutput = self._set_fnameoutput()
-
-    def plot_clustering_coefficient(self, 
-                                    ax, 
+    def compute_clustering_coefficient(self, 
                                     mode: str = "gcc") -> None:
         '''
         Plot global clustering coefficient
 
         Parameters
         ----------
-        ax : TYPE
-            DESCRIPTION.
         mode: string
             gcc for global clustering 
             avgl for average local clustering
 
-        Returns
-        -------
-        None.
-
         '''
-        self.clust = np.empty(len(self.years))
-        
-        for idx, year in enumerate(self.years):
-            print(f"Analyze clustering - year {year}")
-            finput = glob.glob(self.fnameinput + 
-                               f"/*_year_{year}_maxlag_150.hdf5")[0]
             
-            prb_mat = load_dataset_hdf5(finput, year, index=2)
-            prb_mat = np.maximum(prb_mat, prb_mat.transpose())
-            graph = sample_fuzzy_network(prb_mat)
-            
-            if mode.lower() == "avgl":
-                self.clust[idx] = graph.transitivity_avglocal_undirected(mode="NaN") # Average of the local clustering coefficient
-            elif mode.lower() == "gcc":
-                self.clust[idx] = graph.transitivity_undirected(mode="NaN") # Global clustering coefficient
+        if mode.lower() == "avgl":
+            clust = self.graph.transitivity_avglocal_undirected(mode="NaN") # Average of the local clustering coefficient
+        elif mode.lower() == "gcc":
+            clust = self.graph.transitivity_undirected(mode="NaN") # Global clustering coefficient
     
-        ax.plot(self.years, self.clust)
-        ax.set_ylabel("Clustering")
+        return clust
+
         
     
-    def plot_modularity(self, ax) -> None:
+    def compute_modularity(self) -> None:
         '''
         Plot the modularity score
-
-        Parameters
-        ----------
-        ax : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
-
         '''
-        self.modularity = np.empty(len(self.years))
-        
-        for idx, year in enumerate(self.years):
-            print(f"Analyze clustering - year {year}")
-            finput = glob.glob(self.fnameinput + 
-                               f"/*_year_{year}_maxlag_150.hdf5")[0]
-            
-            prb_mat = load_dataset_hdf5(finput, year, index=2)
-            prb_mat = np.maximum(prb_mat, prb_mat.transpose())
-            graph = sample_fuzzy_network(prb_mat)
-            
-            
-            # Detect communities using the Louvain method
-            communities = graph.community_multilevel()
-            
-            # Compute modularity
-            self.modularity[idx] = graph.modularity(communities.membership)
-            
-            
-        ax.plot(self.years, self.modularity)
-        ax.set_ylabel("Modularity")
         
         
-    def plot_edge_density(self, ax) -> None:
+        # Detect communities using the Louvain method
+        communities = self.graph.community_multilevel()
+        
+        # Compute modularity
+        return self.graph.modularity(communities.membership)
+            
+    
+    def compute_edge_density(self) -> None:
         '''
         Plot the edge density
-
-        Parameters
-        ----------
-        ax : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
-
         '''
-        self.density = np.empty(len(self.years))
-        
-        for idx, year in enumerate(self.years):
-            print(f"Analyze clustering - year {year}")
-            finput = glob.glob(self.fnameinput + 
-                               f"/*_year_{year}_maxlag_150.hdf5")[0]
             
-            prb_mat = load_dataset_hdf5(finput, year, index=2)
-            prb_mat = np.maximum(prb_mat, prb_mat.transpose())
-            graph = sample_fuzzy_network(prb_mat)
+        # Compute 
+        nn = self.graph.vcount()
+        return 2*self.graph.ecount()/(nn*(nn-1))
+    
             
-        
-            # Compute 
-            nn = graph.vcount()
-            self.density[idx] = 2*graph.ecount()/(nn*(nn-1))
-            
-            
-        ax.plot(self.years, self.density)
-        ax.set_ylabel("Edge density", fontsize=self.medium_fontsize)
+    def initialize_modes(self):
+        for mode in self.modes:
+            if mode == "clustering":
+                self.clust = np.empty(len(self.years))
+            elif mode == "modularity":
+                self.modularity = np.empty(len(self.years))
+            elif mode == "edge_density":
+                self.density = np.empty(len(self.years))
+            else:
+                raise ValueError("Type not recognized")
--- Analysis/plot.py
+++ Analysis/plot.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
                 "tas_ssp5_8_5_mri_esm2_0_100_surr", # 8
                 "tas_ssp1_2_6_mri_esm2_0_100_surr"
                 ]
-    finput = folderinput + filelist[9]
+    finput = folderinput + filelist[2]
     # baseline = np.arange(1970,1990)  # 2022,2042    1970,1990
     baseline = np.arange(2022,2042)
     
@@ -50,48 +50,42 @@ if __name__ == "__main__":
     #######################
     #%% Network analysis ##
     #######################
-    # mode = "modularity"   # clustering modularity edge_density
-    # resfolder = "./fig_network_analysis/"
-    # years = np.arange(2022,2101)
+    modes = ["clustering","modularity","edge_density"]   # clustering modularity edge_density
+    resfolder = "./fig_network_analysis/"
+    years = np.arange(2022,2101)
+    # years = np.arange(2022,2040)
     
-    # fig, ax = plt.subplots(1,1, figsize=(5,5))
+    fig, axes = plt.subplots(1,len(modes), figsize=(7*len(modes),5))
     
-    # plot = AnalyzeFuzzyNetwork(finput, years)
-    # if mode == "clustering":
-    #     plot.plot_clustering_coefficient(ax, mode="gcc")
-    # elif mode == "modularity":
-    #     plot.plot_modularity(ax)
-    # elif mode == "edge_density":
-    #     plot.plot_edge_density(ax)
-    # else:
-    #     print("Type not recognized")
+    plot = AnalyzeFuzzyNetwork(finput, years, modes)
+    axes = plot.plot(axes)
                 
-    # filename = finput.split("Output/")[1]                  
-    # plt.savefig(f"{resfolder}{mode}_{filename}.pdf",
-    #             bbox_inches='tight')
+    filename = finput.split("Output/")[1]                  
+    plt.savefig(f"{resfolder}{filename}.pdf",
+                bbox_inches='tight')
     
     #%% Create figure 10 years
-    fig, axes = plt.subplots(2,3, figsize=(15,10), 
-                              subplot_kw={'projection': ccrs.Robinson()})
-    for idx, ax in enumerate(axes.flat[0:(len(init_year)-1)]): #(init_year[:-1]):
-        print(f"Analyzing years: {init_year[idx]}-{init_year[idx+1]}")
-        years = np.arange(init_year[idx]+1, init_year[idx+1]+1)
-        plot = draw_variation_earth_network(ax, 
-                                            finput, 
-                                            years, 
-                                            baseline, 
-                                            variat_percnt, 
-                                            lw_connectivity)
-    # fig.delaxes(axes[1, 2])
-    plt.tight_layout()
+    # fig, axes = plt.subplots(2,3, figsize=(15,10), 
+    #                           subplot_kw={'projection': ccrs.Robinson()})
+    # for idx, ax in enumerate(axes.flat[0:(len(init_year)-1)]): #(init_year[:-1]):
+    #     print(f"Analyzing years: {init_year[idx]}-{init_year[idx+1]}")
+    #     years = np.arange(init_year[idx]+1, init_year[idx+1]+1)
+    #     plot = draw_variation_earth_network(ax, 
+    #                                         finput, 
+    #                                         years, 
+    #                                         baseline, 
+    #                                         variat_percnt, 
+    #                                         lw_connectivity)
+    # # fig.delaxes(axes[1, 2])
+    # plt.tight_layout()
     
-    # Save figure
-    filename = "variat_percnt_" if variat_percnt else "variat_"
-    filename += "width_connect_" if lw_connectivity else "width_variat_"
-    filename += finput.split("Output/")[1]
-    plt.savefig(f"{resfolder}{filename}.png",
-                dpi=300,
-                bbox_inches='tight')
+    # # Save figure
+    # filename = "variat_percnt_" if variat_percnt else "variat_"
+    # filename += "width_connect_" if lw_connectivity else "width_variat_"
+    # filename += finput.split("Output/")[1]
+    # plt.savefig(f"{resfolder}{filename}.png",
+    #             dpi=300,
+    #             bbox_inches='tight')
     # #%% Create figures 30 years
     
     # init_year = np.array([2040,2070,2100])
