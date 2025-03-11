fbf7203a643928e076215a4a194e2fb332d72143
Tomas Scagliarini <tomscag92@gmail.com>
2025-03-11 12:12:18 +0100

    Plot: refactor plot network metrics

--- Analysis/lib/plot/AnalyzeFuzzyNetwork.py
+++ Analysis/lib/plot/AnalyzeFuzzyNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
 
 from lib.misc import (
                 load_edgelist,
@@ -11,62 +12,68 @@ from lib.misc import (
 class AnalyzeFuzzyNetwork():
 
     def __init__(self, 
-                 fnameinput: str, 
-                 years: np.array,
-                 modes: list
+                 fnameinputs: list[str], 
+                 years: np.array
                  ) -> None:
         """
             Initialize oject attributes and create figure
         """
-        self.fnameinput = fnameinput
+        self.fnameinputs = fnameinputs
         self.resfolder = "./fig/"
         self.years = years
+        self.figuresize = (3.5,2.33/1.33)
         
+        self.medium_fontsize = 18
+        self.lw = 1.1
         
-        self.medium_fontsize = 20
-        
-        self.modes = modes
-        self.initialize_modes()
+        self.legend = ["awi_cm_1_1_mr", "mri_esm_2.0", "CESM2"]
+        self.arr = np.empty(len(self.years))
 
         
     def plot(self, 
-             axes):
+                        mode: str
+             ):
         
-        for idx, year in enumerate(self.years):
-            print(f"Analyze - year {year}")
-            finput = glob.glob(self.fnameinput + 
-                               f"/*_year_{year}_maxlag_150.hdf5")[0]
-            
-            prb_mat = load_dataset_hdf5(finput, year, index=2)
-            prb_mat = np.maximum(prb_mat, prb_mat.transpose())
-            self.graph = sample_fuzzy_network(prb_mat)
-            
-            for mode in self.modes:
-                if mode == "clustering":
-                    self.clust[idx] = self.compute_clustering_coefficient(mode="gcc")
-                elif mode == "modularity":
-                    self.modularity[idx] = self.compute_modularity()
-                elif mode == "edge_density":
-                    self.density[idx] = self.compute_edge_density()
+        #/ Define figure
+        fig, ax = plt.subplots(1, 1, figsize=self.figuresize, tight_layout = {'pad': .3})
+        ax.set_ylabel(f"{mode}", fontsize=self.medium_fontsize)
+        
+        #/ Specify colors and markers
+        colors = ["#1f77b4", "#d62728", "#2ca02c"]
+        
+        window_size = 6
+        weights = np.ones(window_size) / window_size
+        
+        for (m, fnameinput) in enumerate(self.fnameinputs):
+            for idx, year in enumerate(self.years):
+                print(f"Analyze - year {year}")
+                finput = glob.glob(fnameinput + 
+                                   f"/*_year_{year}_maxlag_150.hdf5")[0]
+                
+                prb_mat = load_dataset_hdf5(finput, year, index=2)
+                prb_mat = np.maximum(prb_mat, prb_mat.transpose())
+                self.graph = sample_fuzzy_network(prb_mat)
+                
+                if mode == "Clustering":
+                    self.arr[idx] = self.compute_clustering_coefficient(mode="gcc")
+                elif mode == "Modularity":
+                    self.arr[idx] = self.compute_modularity()
+                elif mode == "Edge density":
+                    self.arr[idx] = self.compute_edge_density()
                 else:
                     print("Type not recognized")
-        
-        for idx, mode in enumerate(self.modes):
             
-            if mode == "clustering":
-                axes[idx].plot(self.years, self.clust)
-                axes[idx].set_ylabel("Clustering", fontsize=self.medium_fontsize)
-            elif mode == "modularity":
-                axes[idx].plot(self.years, self.modularity)
-                axes[idx].set_ylabel("Modularity", fontsize=self.medium_fontsize)
-            elif mode == "edge_density":
-                axes[idx].plot(self.years, self.density)
-                axes[idx].set_ylabel("Edge density", fontsize=self.medium_fontsize)
-            else:
-                raise ValueError("Type not recognized")
+            # sma = np.convolve(self.arr, weights, mode='same') # valid
+            ax.plot(self.years, self.arr, linewidth=self.lw, 
+                    color=colors[m], label=self.legend[m])
+        ax.legend(fontsize=9, loc="upper right", handlelength=1, frameon=False)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
                 
-        return axes
+        return ax
             
+
+    
 
     def compute_clustering_coefficient(self, 
                                     mode: str = "gcc") -> None:
@@ -113,13 +120,3 @@ class AnalyzeFuzzyNetwork():
         return 2*self.graph.ecount()/(nn*(nn-1))
     
             
-    def initialize_modes(self):
-        for mode in self.modes:
-            if mode == "clustering":
-                self.clust = np.empty(len(self.years))
-            elif mode == "modularity":
-                self.modularity = np.empty(len(self.years))
-            elif mode == "edge_density":
-                self.density = np.empty(len(self.years))
-            else:
-                raise ValueError("Type not recognized")
